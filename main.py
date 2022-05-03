b7fd463ea95d8e02246a698f22e934c3a8981ae0
manealucacristian <luca.cmanea@gmail.com>
2022-05-03 16:54:25 -0300

    request_response_body

--- main.py
+++ main.py
@@ -1,8 +1,34 @@
+#python
+from typing import Optional
+
+#Pydantic
+from pydantic import BaseModel
+#Pydantic
 from fastapi import FastAPI
+from fastapi import Body
+
 #app contiene toda la aplicacion como una instancia de FasAPI
+
 app = FastAPI()
+
+#Models
+class Person(BaseModel):
+    first_name: str
+    last_name: str
+    age: int
+    hair_color: Optional[str] =None
+    is_married: Optional[bool] = None
+    
+
+
 
 @app.get("/")
 def home():
     return {"hello":"world"}
 
+ #Reques and Response body
+ #Body(...) nos indica que el parametro que enviamos es obligatorio
+@app.post("/person/new")
+def create_person(person: Person  = Body(...)):
+     return person
+ 

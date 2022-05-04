22012d0cc42d453392c70de0c9f3d88ff966060f
manealucacristian <luca.cmanea@gmail.com>
2022-05-04 00:34:03 -0300

    Automatic_path_and_qiety_examples

--- main.py
+++ main.py
@@ -64,15 +64,19 @@ def create_person(person: Person  = Body(...)):
 @app.get("/person/detail")
 def show_person(name:Optional[str] = Query(None,min_length=1,max_length= 50,
                 title="Person Name", description="This is the persona name. Its between 1 and 50 characters"),
+                example="Christian",
                 age: str = Query(..., title="Person Age",
-                                 description="This is the person age its Required")):
+                                 description="This is the person age its Required",
+                                 example=1)):
     return {name:age}
 
 #validaciones: Path paramaeters
 
 @app.get("/person/detail/{person_id}")
 def show_person(person_id:int = Path(..., gt=0, title= "Person Id",
-                    description= "This is the Person Id its Required")):
+                    description= "This is the Person Id its Required",
+                    example=110)):
+    
     return {person_id:"It Exist!"}
  
 @app.put("/person/{person_id}")
@@ -80,7 +84,7 @@ def update_person(
     person_id: int = Path(...,
                           title="Person Id",
                           description="This is the person id",
-                          gt=0),
+                          gt=0,example=112),
         person:Person = Body(...),
         location: Location = Body(...)
         ):
